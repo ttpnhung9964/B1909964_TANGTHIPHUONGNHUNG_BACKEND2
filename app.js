5afa852889eff9a0106e607b1b0bc71ac80f9b10
ttpnhung9964 <122326427+ttpnhung9964@users.noreply.github.com>
2024-01-09 12:21:27 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+
+const contactsRouter = require("./app/routes/contact.route");
+
+const ApiError = require("./app/api-error");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/contacts", contactsRouter);
+app.use((err, req, res, next) => {
+  return res.status(error.statusCode || 500).json({
+    message: error.message || "Internal Server Error",
+  });
+});
+app.use((req, res, next) => {
+  return next(new ApiError(404, "Resource not found"));
+});
+
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to contact book application." });
+});
+
+module.exports = app;
